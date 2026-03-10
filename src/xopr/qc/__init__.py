bdd52415e81dd0ca10d6b1f3cb27435f11bea5d8
Thomas Teisberg <tteisberg@gmail.com>
2026-03-09 18:43:56 -0700

    Add new QC checks

--- src/xopr/qc/__init__.py
+++ src/xopr/qc/__init__.py
@@ -1,7 +1,17 @@
 """Quality control module for polar radar datasets."""
 
 from .checks import ensure_picks as ensure_picks
+from .checks import heading_change as heading_change
 from .checks import ice_thickness_threshold as ice_thickness_threshold
+from .checks import snr_bed_pick as snr_bed_pick
+from .checks import minimum_agl as minimum_agl
 from .runner import run_qc as run_qc
 
-__all__ = ["ensure_picks", "ice_thickness_threshold", "run_qc"]
+__all__ = [
+    "ensure_picks",
+    "heading_change",
+    "ice_thickness_threshold",
+    "run_qc",
+    "snr_bed_pick",
+    "minimum_agl",
+]
